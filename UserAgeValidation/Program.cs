79acc1c9189fc4e537d24c7afafdad550a6f914e
Magic Clip <magic@clip.com>
2024-04-12 09:57:27 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus küsib kasutajal sisestada tema vanus
+//Kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//Muul juhul
+//Konsoolis kuvatakse "Welcome to Instagram"
+
+using System;
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+
+if(isAgeNumber == true)
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young too use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
+
---
+++ Week2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//Uued andme tüübid:int ja char
+//if-statements (control flow)
+
+
